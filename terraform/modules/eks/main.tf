4655b655a17bff098d2853799cdc46797392f3d2
narendra-clouds <deshmukhn298@gmail.com>
2026-09-07 10:07:38 +0530

    added

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.31"
+  version  = "1.32"
 
   vpc_config {
     subnet_ids              = concat(var.public_subnet_ids, var.private_subnet_ids)
